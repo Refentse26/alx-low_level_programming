f853f1a6d60e0e61b0a52bb266c66313a3e007a7
Your Name <you@example.com>
2023-02-16 14:00:07 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - print out sizes of data types in c
+ * Return: 0
  */
 int main(void)
 {
@@ -11,3 +11,4 @@ int main(void)
 	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("size of a float: %i byte(s)\n", sizrof(float));
 	return (0);
+}
