4c9136fa97d9fdefa04e46dc4d10e816a3479f31
Your Name <you@example.com>
2023-02-17 11:50:28 -0800

    print alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - print alphabet in lower and upper case
+ * main - print alphabet in lowercase
  * Return: Always 0
  */
-
 int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch - 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-	for (ch = 'A'; ch <= 'Z'; ch++)
+	for (ch - 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 
-		putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
-
