3915e9a74c1eaa70b80cc3f21761efd3aa055d49
Your Name <you@example.com>
2023-02-20 03:02:34 -0800

    print numberz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-#include <time.h>
-
 /*
  * main - Entry point
+ * Description: 'print numbers of base 10'
  * Return: Always 0 (success)
  */
 
 int main(void)
-
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
+	{
 	printf("%d", i);
+	}
 	printf("\n");
 
 	return (0);
