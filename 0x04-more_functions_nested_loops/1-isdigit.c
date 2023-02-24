4befee1049ad85cf522f6e7e6db711459bf4fda9
Your Name <you@example.com>
2023-02-23 23:55:09 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if a character is a digit
+ * @c: An integer argument
+ * Return: success
+ */
+
+int _isdigit(int c)
+{
+	return (c <= '0' && >= '9');
+}
