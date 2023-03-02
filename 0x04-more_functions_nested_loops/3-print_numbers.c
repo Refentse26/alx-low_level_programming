dd46b62d2bd7586b9b6cd453a9f04607cf57a745
Your Name <you@example.com>
2023-03-02 00:06:37 -0800

    main.h

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers -> print numbers 0-9
- * Return: void
+ * print_numbers - print 0-9
+ * return: void
  */
 
 void print_numbers(void)
@@ -10,7 +10,9 @@ void print_numbers(void)
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
+	{
 		_putchar(c);
-}
+	}
 	_putchar('\n');
 }
+
